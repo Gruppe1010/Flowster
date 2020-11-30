9581c3509fa79897132bf8a24d2d3187a1a9cc06
RRoager <rasmus-roager@hotmail.com>
2020-11-30 10:40:21 +0100

    Initial commit

---
+++ src/main/java/gruppe10/flowster/FlowsterApplication.java
@@ -0,0 +1,13 @@
+package gruppe10.flowster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowsterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowsterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gruppe10/flowster/FlowsterApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppe10.flowster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowsterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
