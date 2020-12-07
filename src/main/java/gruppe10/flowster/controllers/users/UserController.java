ec1a2d27c60fbbcfdb8cb605ada30924103cb0b0
TobiasBojesen <tobiasbojesen@hotmail.dk>
2020-12-07 15:04:08 +0100

    Ny model i controller

--- src/main/java/gruppe10/flowster/controllers/users/UserController.java
+++ src/main/java/gruppe10/flowster/controllers/users/UserController.java
@@ -82,9 +82,10 @@ public class UserController
     }
     
     @GetMapping("/createUser")
-    public String logIn(Model createUserViewModel)
+    public String logIn(Model createUserViewModel, Model loggedInUserModel)
     {
         createUserViewModel.addAttribute( "createUserViewModel", this.createUserViewModel);
+        createUserViewModel.addAttribute( "loggedInUser", loggedInUserModel);
 
         return "general/create-user"; // html
 
